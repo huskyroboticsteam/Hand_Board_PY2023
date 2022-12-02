48fd37fb320d6dc1f9650b518eb090acae02baf5
liamk981 <liamk981@uw.edu>
2022-12-01 18:54:14 -0800

    12/1
    Working on CAN method

--- CAN.cydsn/main.c
+++ CAN.cydsn/main.c
@@ -18,9 +18,15 @@ int main(void)
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
     for(;;)
+    
     {
         /* Place your application code here. */
     }
+    
 }
+
+// CAN method
+// Set up can packet logic
+
 
 /* [] END OF FILE */
