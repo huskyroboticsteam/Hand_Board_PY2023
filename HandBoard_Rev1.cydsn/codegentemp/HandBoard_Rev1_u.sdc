102f4f03124190288956c87c52d11943d5a8175c
liamk981 <liamk981@uw.edu>
2023-02-06 18:50:20 -0800

    Need to fix errors with the limit switches on top design

--- HandBoard_Rev1.cydsn/codegentemp/HandBoard_Rev1_u.sdc
+++
@@ -1,3 +0,0 @@
-# Component constraints for C:\Users\kratz\OneDrive\Documents\PSoC Creator\Hand_Board_PY2023\HandBoard_Rev1.cydsn\TopDesign\TopDesign.cysch
-# Project: C:\Users\kratz\OneDrive\Documents\PSoC Creator\Hand_Board_PY2023\HandBoard_Rev1.cydsn\HandBoard_Rev1.cyprj
-# Date: Tue, 24 Jan 2023 02:07:25 GMT
--- HandBoard_Rev1.cydsn/main.c
+++ HandBoard_Rev1.cydsn/main.c
@@ -1,18 +1,7 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
+// Main c file for HandBoard 2023
+
 #include "main.h" 
-#include "Pin_Limit_1.h"
-#include "Pin_Limit_2.h"
-#include "Pin_Encoder_A.h"
+
 
 int main(void){
   Initialize();
@@ -59,6 +48,59 @@ void Initialize(void) {
 
     isr_Limit_1_StartEx(Pin_Limit_Handler);
     isr_period_PWM_StartEx(Period_Reset_Handler);
+} 
+
+CY_ISR(Pin_Limit_Handler){
+    #ifdef PRINT_LIMIT_SW_TRIGGER
+    sprintf(txData,"LimitSW triggerd Stat: %x \r\n", Status_Reg_Switches_Read() & 0b11);
+    UART_UartPutString(txData);
+    #endif
+    
+    set_PWM(GetCurrentPWM(), ignoreLimSw, Status_Reg_Switches_Read());
+    
+    #ifdef CAN_TELEM_SEND
+    AssembleLimitSwitchAlertPacket(&can_send, DEVICE_GROUP_JETSON, 
+        DEVICE_SERIAL_JETSON, Status_Reg_Switches_Read() & 0b11);
+    SendCANPacket(&can_send);
+    #endif
+    //TODO: Select Which Encoder zeros
+    //QuadDec_SetCounter(0);
+    if (bound_set1 && ~Pin_Limit_1_Read()) {
+        QuadDec_SetCounter(enc_lim_1);
+    } else {
+        QuadDec_SetCounter(enc_lim_2);
+    }
 }
 
-/* [] END OF FILE */
+CY_ISR(Period_Reset_Handler) {
+    int timer = Timer_PWM_ReadStatusRegister();
+    invalidate++;
+    CAN_time_LED++;
+    CAN_check_delay ++;
+    ERRORTimeLED++;
+    encoderTimeOut++;
+    if (encoderTimeOut >= 2){
+        encoderTimeOut = 0;
+        SendEncoderData(&can_send);
+    }
+    if (invalidate >= 20){
+        set_PWM(0, 0, 0);   
+    }
+    if (ERRORTimeLED >= 3) {
+        #ifdef ERROR_LED
+        ERROR_LED_Write(LED_OFF);
+        #endif
+        #ifdef DEBUG_LED1   
+        Debug_1_Write(LED_OFF);
+        #endif
+        #ifdef DEBUG_LED2
+        Debug_2_Write(LED_OFF);
+        #endif
+    }
+    if (CAN_time_LED >= 3){
+        #ifdef CAN_LED
+        CAN_LED_Write(LED_OFF);
+        #endif
+    }
+}
+
